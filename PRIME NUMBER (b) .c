4045dd6b7144ad561dbbc2a1412a35bd4eb971cd
Ananya2676 <ananyahv26@gmail.com>
2024-08-31 14:33:37 +0530

    Create PRIME NUMBER (b) .c

---
+++ PRIME NUMBER (b) .c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int n,flag=0,i,j;
+  printf("enter the upper limit:");
+  scanf("%d",&n);
+  for(i=2;i<=n;i++){
+    flag=0;
+    for(j=2;j<i;j++){
+      if(i%j==0){
+        flag = 1;
+        break;
+      }
+    }
+  if(flag==0)
+  {
+    printf("%d is a prime number.\n",i);
+  }
+  else
+  {
+    printf("%d is not a prime number.\n",j);
+  }
+}
+}    
