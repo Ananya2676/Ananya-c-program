804c4863348fbed632ff0f419f346e6668b691c3
Ananya2676 <ananyahv26@gmail.com>
2024-09-01 23:01:33 +0530

    Create CREATION (no output) .c

---
+++ CREATION (no output) .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+int data;
+struct node*next;
+};
+struct node*createnode(int data)
+{
+  struct node*newnode=(struct node*)malloc(sizeof(struct node));
+  newnode->data=data;
+  newnode->next=NULL;
+  return newnode;
+}
